17a6f89c1ae64fab6740440ec9c1f71e8f130c72
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-02 17:33:49 -0300

    📝docs: add Swagger UI route to serve API documentation
    Added the Swagger UI route (GET /swagger/*any) to the router using gin-swagger and swaggo files.
    This change enables easy access to the auto-generated Swagger documentation for the API.
    
    Also imported the "docs" package to load Swagger annotations.

--- router/router.go
+++ router/router.go
@@ -1,9 +1,15 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	_ "github.com/alysonsz/gopportunities.git/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
-func Router() { //O nome da função importa, pois se fosse router(), ela seria uma função local, e não seria possível exportar
+func Router() {
 	router := gin.Default()
 	initializeRoutes(router)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":3030")
 }

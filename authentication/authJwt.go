01e16cca573d4d9452c5c73d644993350e271e19
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-02 17:39:47 -0300

    ✨feat: add JWT authentication middleware
    - Added AuthJwt middleware for validating JWT tokens in incoming HTTP requests.
    - Ensures routes are secured and user context is set based on valid tokens.

---
+++ authentication/authJwt.go
@@ -0,0 +1,38 @@
+package authentication
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/alysonsz/gopportunities.git/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthJwt() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
+			c.Abort()
+			return
+		}
+
+		token := tokenParts[1]
+		claims, err := utils.ValidateJWT(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", claims["user_id"])
+		c.Next()
+	}
+}

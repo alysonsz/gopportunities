ec3e463ddfdabfa2736cd5e89279c52e52d99710
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-02 17:41:17 -0300

    ✨feat: integrate authentication & protect opportunity routes
    - Added AuthController with /register and /login routes.
    - Applied AuthJwt middleware to protect opportunity routes.
    - Included User model in database migration.
    - Enhanced database connection handling.
    
    This update ensures authenticated access to opportunity endpoints and integrates full authentication flow.

--- router/routes.go
+++ router/routes.go
@@ -1,34 +1,45 @@
 package router
 
 import (
+	"github.com/alysonsz/gopportunities.git/authentication"
 	"github.com/alysonsz/gopportunities.git/controllers"
 	"github.com/alysonsz/gopportunities.git/models"
 	"github.com/alysonsz/gopportunities.git/repositories"
 	"github.com/alysonsz/gopportunities.git/services"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func initializeRoutes(router *gin.Engine) {
-	db, err := gorm.Open(sqlite.Open("gopportunities.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./gopportunities.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("failed to connect database" + err.Error())
 	}
 
-	// Migração automática da tabela Opportunity
-	db.AutoMigrate(&models.Opportunity{})
+	db.AutoMigrate(&models.Opportunity{}, &models.User{})
 
 	opportunityRepo := repositories.NewOpportunityRepository(db)
+	userRepo := repositories.NewUserRepository(db)
 	opportunityService := services.NewOpportunityService(opportunityRepo)
+	authService := services.NewAuthService(userRepo)
 	opportunityController := controllers.NewOpportunityController(opportunityService)
+	authController := controllers.NewAuthController(authService)
 
 	v1 := router.Group("/api/v1")
 	{
-		v1.POST("/opportunities", opportunityController.Create)
-		v1.GET("/opportunities/:id", opportunityController.Get)
-		v1.GET("/opportunities", opportunityController.GetAll)
-		v1.PUT("/opportunities/:id", opportunityController.Update)
-		v1.DELETE("/opportunities/:id", opportunityController.Delete)
+		v1.POST("/register", authController.Register)
+		v1.POST("/login", authController.Login)
+
+		authRoutes := v1.Group("/")
+		authRoutes.Use(authentication.AuthJwt())
+		{
+			authRoutes.POST("/opportunities", opportunityController.Create)
+			authRoutes.GET("/opportunities/:id", opportunityController.Get)
+			authRoutes.GET("/opportunities", opportunityController.GetAll)
+			authRoutes.PUT("/opportunities/:id", opportunityController.Update)
+			authRoutes.DELETE("/opportunities/:id", opportunityController.Delete)
+		}
 	}
 }
